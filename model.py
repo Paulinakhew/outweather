d98fbb4bad84aedc9e443b3a4997913bed5a0433
Your Name <you@example.com>
2019-02-23 14:53:56 -0500

    implemented class method for retrieving weather info 💯

--- model.py
+++ model.py
@@ -1,18 +1,19 @@
 import requests
 
-def getWeather():
-    hourlyWeather = {}
-    weatherData = requests.get('api.openweathermap.org/data/2.5/forecast?q=%s&APPID=225800a064f12ac6d7fbaadf83ce753f' %location)
-    weatherData = weatherData.json()
-    for x in range(8):
-        dateTime = weatherData.list[x].dt_txt
-        timeArr = dateTime.rsplit(" ")
-        time = timeArr[1]
-        hourlyWeather[time] = []
-        hourlyWeather[time].append({"Temperature": weatherData.list[x].main.temp - 273.15})
-        hourlyWeather[time].append({"Weather": weatherData.list[x].weather[0].main})
-        hourlyWeather[time].append({"Weather Details": weatherData.list[x].weather[0].description})
-        hourlyWeather[time].append({"Wind Speed": weatherData.list[x].wind.speed})
-    
-    return hourlyWeather
+class WeatherInfo:
+    def getWeather():
+        hourlyWeather = {}
+        weatherData = requests.get('api.openweathermap.org/data/2.5/forecast?q=%s&APPID=225800a064f12ac6d7fbaadf83ce753f' %location)
+        weatherData = weatherData.json()
+        for x in range(8):
+            dateTime = weatherData.list[x].dt_txt
+            timeArr = dateTime.rsplit(" ")
+            time = timeArr[1]
+            hourlyWeather[time] = []
+            hourlyWeather[time].append({"Temperature": weatherData.list[x].main.temp - 273.15})
+            hourlyWeather[time].append({"Weather": weatherData.list[x].weather[0].main})
+            hourlyWeather[time].append({"Weather Details": weatherData.list[x].weather[0].description})
+            hourlyWeather[time].append({"Wind Speed": weatherData.list[x].wind.speed})
+        
+        return hourlyWeather
 
