d8dca50480adb1d1c08da3d14ebb8a28c097b734
Your Name <you@example.com>
2019-02-23 18:23:05 -0500

    added outfits page to the backend

--- controller.py
+++ controller.py
@@ -21,7 +21,17 @@ def weather():
     else:
         submitted_city=request.form['city']
         result = m.getWeather(submitted_city)
-        return render_template('weather.html', result = result)
+        return render_template('weather.html')
 
+@app.route('/outfits', methods=['GET', 'POST'])
+def outfit():
+    if request.method == "GET":
+        return render_template('outfits.html')
+    
+    else:
+        result = m.getWeather(submitted_city)
+        return render_template('outfits.html')
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
