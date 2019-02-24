a7a111c1ba21a270dee1c3413236ce9a2c3f9605
Your Name <you@example.com>
2019-02-23 23:26:07 -0500

    updating information arrays

--- controller.py
+++ controller.py
@@ -32,17 +32,12 @@ def weather():
         cold_footwear = ["Boots", "Hiking Boots", "Uggs", "Winter Moccasins", "Leather shoes"]
         cold_top = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Fur Coat"]
         cold_accessories = ["Hat", "Touque", "Mittens", "Gloves", "Scarf", "Thermal Base Layers"]
-        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings", ""]
+        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings", "Khakis"]
 
-        mild_footwear = ["Running Shoes", "Dress Shoes", "Slip-On Casual Shoes"]
+        mild_footwear = ["Running Shoes", "Dress Shoes", "Slip-On Casual Shoes", "Flip-Flops", "Heels"]
         mild_top = ["T-Shirt", "Long-Sleeve Shirt", "Light Sweatshirt", "Jean Jacket", "Dress Shirt"]
         mild_accessories = ["Baseball Cap", "Sunglasses"]
         mild_bottoms = ["Sweatpants", "Long Skirt", "Jeans", "Cargo Pants", "Dress Pants", "Leggings"]
-
-        snowRain_footwear = ["Boots", "Hiking Shoes", "Rain Boots"]
-        snowRain_top = ["Jacket", "Parka", ]
-        snowRain_accessories = ["Hat", "Touque", "Gloves", "Scarf"]
-        snowRain_bottoms = ["Sweatpants", "Jeans", "Leggings", ]
 
         hot_footwear = ["Flip-Flops", "Sandals", "Slides", "Running Shoes", "Slip-On Casual Shoes"]
         hot_top = ["Tank Top", "T-Shirt", "Undershirt"]
@@ -52,7 +47,6 @@ def weather():
         return render_template('outfits.html', city = submitted_city, result = result, randomInt = random_num, coldFootwear = cold_footwear,
         coldBottoms = cold_bottoms, coldTop = cold_top, coldAccessories = cold_accessories,
         mildFootwear = mild_footwear, mildTop = mild_top, mildAccessories = mild_accessories, mildBottoms = mild_bottoms,
-        precipFootwear = snowRain_footwear, precipTop = snowRain_top, precipAccessories = snowRain_accessories, precipBottom = snowRain_bottoms,
         hotFootwear = hot_footwear, hotTop = hot_top, hotAccessories = hot_accessories, hotBottoms = hot_bottoms)
 
 if __name__ == '__main__':
