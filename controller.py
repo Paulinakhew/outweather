c0fcef58e836145b1a960af033e47bead748be75
Your Name <you@example.com>
2019-02-23 23:08:50 -0500

    beginning to add conditionals for diaplying information

--- controller.py
+++ controller.py
@@ -23,15 +23,19 @@ def weather():
     else:
         submitted_city = request.form['city']
         result = m.getWeather(submitted_city)
-        cold_footwear = ["Boots", "Hiking Boots", "Uggs", "Winter Moccasins"]
-        cold_top = ["Jacket", "Parka", ]
+
+        random.seed(datetime.now())
+        random_num = random.randint(0, 4)
+        
+        cold_footwear = ["Boots", "Hiking Boots", "Uggs", "Winter Moccasins", "Leather shoes"]
+        cold_top = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Fur Coat"]
         cold_accessories = ["Hat", "Touque", "Mittens", "Gloves", "Scarf", "Thermal Base Layers"]
-        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings"]
+        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings", ""]
 
         mild_footwear = ["Running Shoes", "Dress Shoes", "Slip-On Casual Shoes"]
         mild_top = ["T-Shirt", "Long-Sleeve Shirt", "Light Sweatshirt", "Jean Jacket", "Dress Shirt"]
         mild_accessories = ["Baseball Cap", "Sunglasses"]
-        mild_bottoms = ["Sweatpants", "Trackpants", "Jeans", "Cargo Pants", "Dress Pants", "Leggings"]
+        mild_bottoms = ["Sweatpants", "Long Skirt", "Jeans", "Cargo Pants", "Dress Pants", "Leggings"]
 
         snowRain_footwear = ["Boots", "Hiking Shoes", "Rain Boots"]
         snowRain_top = ["Jacket", "Parka", ]
@@ -41,10 +45,9 @@ def weather():
         hot_footwear = ["Flip-Flops", "Sandals", "Slides", "Running Shoes", "Slip-On Casual Shoes"]
         hot_top = ["Tank Top", "T-Shirt", "Undershirt"]
         hot_accessories = ["Fan", "Water Bottle"]
-        hot_bottoms = ["Shorts", "Cargo Shorts", "Jean Shorts", "Trackpants", "3/4 Length Trackpants", ]
-        random.seed(datetime.now())
-        
-        return render_template('outfits.html', city = submitted_city, result = result, coldFootware = cold_footwear,
+        hot_bottoms = ["Short Skirt", "Cargo Shorts", "Jean Shorts", "Trackpants", ""]
+
+        return render_template('outfits.html', city = submitted_city, result = result, randomInt = random_num, coldFootware = cold_footwear,
         coldBottoms = cold_bottoms, coldTop = cold_top, coldAccessories = cold_accessories,
         mildFootwear = mild_footwear, mildTop = mild_top, mildAccessories = mild_accessories, mildBottoms = mild_bottoms,
         precipFootwear = snowRain_footwear, precipTop = snowRain_top, precipAccessories = snowRain_accessories, precipBottom = snowRain_bottoms,
