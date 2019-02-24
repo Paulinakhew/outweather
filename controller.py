c72b5a0e6f2da4305bf67a9ad69999eebf081d30
Your Name <you@example.com>
2019-02-23 20:12:28 -0500

    adding outfit option arrays

--- controller.py
+++ controller.py
@@ -21,6 +21,26 @@ def weather():
     else:
         submitted_city = request.form['city']
         result = m.getWeather(submitted_city)
+        cold_footwear = ["Boots", "Hiking Boots"]
+        cold_top = ["Jacket", "Parka", ]
+        cold_accessories = ["Hat", "Touque", "Mittens", "Gloves", "Scarf", "Thermal Base Layers"]
+        cold_bottoms = ["Jeans", "Sweatpants"]
+
+        mild_footwear = ["Running Shoes", "Dress Shoes", ""]
+        mild_top = ["T-Shirt", "Long-Sleeve Shirt", "Light Sweatshirt"]
+        mild_accessories = []
+        mild_bottoms = ["Sweatpants", "Trackpants", "Jeans", "Cargo Pants"]
+
+        snowRain_footwear = ["Boots"]
+        snowRain_top = []
+        snowRain_accessories = []
+        snowRain_bottoms = []
+
+        hot_footwear = []
+        hot_top = []
+        hot_accessories = []
+        hot_bottoms = []
+
         return render_template('outfits.html', city = submitted_city, result = result)
     
 if __name__ == '__main__':
